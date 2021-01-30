6066b80d77f466e665c76ed3f16846a8cac7b64c
Abdansb <78070587+Abdansb@users.noreply.github.com>
2021-01-30 23:54:02 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 cd %~dp0
 cls
 
-SRBMiner-MULTI --disable-gpu --algorithm yespower2b --pool stratum-ru.rplant.xyz:7022 --wallet MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD
+SRBMiner-MULTI.exe --disable-gpu --algorithm yespower2b --pool stratum-ru.rplant.xyz:7022 --wallet MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD
 pause
