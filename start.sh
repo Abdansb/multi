00d8e7e8ddbeaeef37b130e02556e394c6aee77d
Abdansb <78070587+Abdansb@users.noreply.github.com>
2021-01-30 23:52:56 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
-:: This is an example you can edit and use
-:: There are numerous parameters you can set, please check Help and Examples folder
-
 @echo off
 cd %~dp0
 cls
 
-SRBMiner-MULTI.exe --disable-gpu --algorithm yespower2b --pool stratum-ru.rplant.xyz:7022 --wallet MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD
+SRBMiner-MULTI --disable-gpu --algorithm yespower2b --pool stratum-ru.rplant.xyz:7022 --wallet MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD
 pause
