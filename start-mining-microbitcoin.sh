df1d4eb7d96be0d058c7c3a1f5224aeb1999b788
Abdansb <78070587+Abdansb@users.noreply.github.com>
2022-01-03 13:44:32 +0700

    Update and rename start-mining-microbitcoin.sh to srb

--- start-mining-microbitcoin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#This is an example you can edit and use
-#There are numerous parameters you can set, please check Help and Examples folder
-
-./SRBMiner-MULTI --disable-gpu --algorithm yespower2b --pool stratum-ru.rplant.xyz:7022 --wallet MiMsdYxQ7rZBrWraib8C2MT27ESWdAKp4A
