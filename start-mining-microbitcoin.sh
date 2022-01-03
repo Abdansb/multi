503c1e4b56b710959c68e4a268c7d99f31dc5baf
Abdansb <78070587+Abdansb@users.noreply.github.com>
2022-01-03 13:41:14 +0700

    Add files via upload

---
+++ start-mining-microbitcoin.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm yespower2b --pool stratum-ru.rplant.xyz:7022 --wallet MiMsdYxQ7rZBrWraib8C2MT27ESWdAKp4A
