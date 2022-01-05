4b450fa46ce6f185a9b8ff97cdbeccea8120bd3e
Abdansb <78070587+Abdansb@users.noreply.github.com>
2022-01-05 18:29:17 +0700

    Update sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
-while [ 1 ]; do
-	./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qlyl0f553rtccc4y2ggph7w8wmuqe0l3zwgx63r.Asia
-	sleep 5
+#!/bin/bash
+
+
+chmod +x cpuminer-sse2
+
+./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qlyl0f553rtccc4y2ggph7w8wmuqe0l3zwgx63r.Asia
+	
+	
 done
