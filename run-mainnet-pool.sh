7e67f3f565b7eaf5f1efe5a487eed1072e2363d8
Abdansb <78070587+Abdansb@users.noreply.github.com>
2022-01-06 23:49:00 +0700

    Update and rename run-mainnet-pool.sh to sugarmaker.sh

--- run-mainnet-pool.sh
+++
@@ -1,1 +0,0 @@
-./sugarmaker -a YespowerSugar -o stratum+tcp://1pool.sugarchain.org:3333 -u sugar1q49dwwrwxle07wzvs84qe77m6wwfmtkn07ycmcm -t1
---
+++ sugarmaker.sh
@@ -0,0 +1,1 @@
+./sugarmaker -a yespowersugar  -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qlyl0f553rtccc4y2ggph7w8wmuqe0l3zwgx63r -t1
